676fad9e58ad1f8814578a3e3124541ffadcc202
dawood-d <md.dawood.dawood.786@gmail.com>
2023-07-24 10:21:24 +0000

    mistake-solution

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,10 @@
+// Write your code here
+import './index.css'
+
+const NotFound = () => (
+  <div className="not-found-route-container">
+    <h className="not-found-heading">Page Not Found !!</h>
+  </div>
+)
+
+export default NotFound
